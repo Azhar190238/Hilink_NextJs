79b4cab059999f77a32df5723fa5ef6eceab30eb
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 19:47:49 +0600

    Alhamdulillah updated

--- components/FeelingLost.jsx
+++ components/FeelingLost.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { TfiBook } from "react-icons/tfi";
