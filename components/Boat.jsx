3697d37ebcd5fb1b190ed9462c577a3bacd30b81
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 21:45:47 +0600

    Alhamdulillah updated

--- components/Boat.jsx
+++ components/Boat.jsx
@@ -5,7 +5,8 @@ const Boat = () => {
     return (
         <div className=''>
          <div>
-         <Image src="/boat.png" height={1000} width={1000} alt="Camp Picture" />
+        
+        
          </div>
         </div>
     );
