7d070fc6e9474f49f0ca6d8f6249f7624b333095
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 19:45:53 +0600

    Alhamdulillah updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -74,11 +74,7 @@ export default Hero;
 
 
 
-//                     <div className="flex mt-6 space-x-4">
-//                         <div className="rating">
-//                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-//                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-//                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            
 //                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
 //                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked />
 //                         </div>
