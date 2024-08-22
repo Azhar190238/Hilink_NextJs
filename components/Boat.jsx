ad4e8d217e1adcdd9707b118d7ecae298cf5a489
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 21:48:54 +0600

    Alhamdulillah updated

--- components/Boat.jsx
+++ components/Boat.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 const Boat = () => {
     return (
-        <div className=''>
+        <div className='mb-10'>
             <div>
 
                 <Image
                     src="/boat.png"
                     alt="Camp Picture"
-                    layout="responsive"
+                    layout="responsive" // for full width
                     width={1000}
                     height={1000}
                      className='rounded-3xl'
