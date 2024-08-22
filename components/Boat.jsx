473cac0c6509b5bb2cba9a281f86428aca06933c
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 21:45:22 +0600

    Alhamdulillah updated

--- components/Boat.jsx
+++ components/Boat.jsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Boat = () => {
     return (
-        <div>
-         
+        <div className=''>
+         <div>
+         <Image src="/boat.png" height={1000} width={1000} alt="Camp Picture" />
+         </div>
         </div>
     );
 };
