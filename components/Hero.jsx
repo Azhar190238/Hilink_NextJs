72900b9fb073561b75401323e3e4ae901a248219
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-22 19:42:54 +0600

    Alhamdulillah updated

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -20,12 +20,7 @@
 //                         </div>
 //                     </div>
 //                 </div>
-//                 <div className="flex-1">
-//                     <Image src="/camp.svg" height={50} width={50} alt="Camp Picture"></Image>
-//                     <h1 className="font-bold text-3xl md:text-7xl text-black -mt-3">
-//                         Putuk Truno
-//                         <span className="block mt-6">Camp Area</span>
-//                     </h1>
+
 //                     <p className="mt-6 text-gray-30 max-w-[520px]">
 //                         We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
 //                     </p>
